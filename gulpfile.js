28aecbd28f38640056373ef4dab6b8af10a58c3e
Javier Fernandes <javier.fernandes@gmail.com>
2016-05-21 12:43:19 -0300

    Haciendo que gulp falle cuando falla jshint

--- gulpfile.js
+++ gulpfile.js
@@ -6,5 +6,6 @@ var stylish = require('jshint-stylish');
 gulp.task('lint', function() {
     return gulp.src('hello.js')
         .pipe(jshint())
-        .pipe(jshint.reporter(stylish));
+        .pipe(jshint.reporter(stylish))
+        .pipe(jshint.reporter('fail'));
 });
